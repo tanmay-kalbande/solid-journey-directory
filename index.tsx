0dd1ac749722a7e4f0745b3fac71b36ebf7e0df4
Tanmay Kalbande <123178447+tanmay-kalbande@users.noreply.github.com>
2025-11-23 09:25:34 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'; // 👈 ADDED THIS IMPORT
 import App from './App';
 
 const rootElement = document.getElementById('root');
